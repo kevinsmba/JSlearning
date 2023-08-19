36e7cdd9b189d33fd3afd995837c79da036ca41b
kevinsmba <kevinsmba@gmail.com>
2023-08-18 20:43:54 -0500

    new information added to the scripts.js doc

---
+++ assignents.js
@@ -0,0 +1,20 @@
+
+const country = 'Colombia';
+const continent = 'America';
+let population = 45;
+
+//console.log(country+' is in the '+continent+' and its population is '+population);
+
+const isIsland = false;
+let language = 'Spanish';
+let newPopulation = population+1;
+let FinlandPopulation = 6;
+let comparePopulation = population > FinlandPopulation;
+let averagePopulation = 33;
+let averageComparison = population > averagePopulation;
+
+//console.log(isIsland+' '+newPopulation+' '+averageComparison+' '+language);
+
+let description = `${country} + ' is in '+${continent}+', and its '+${population}+' million people speak '+${language}`;
+
+console.log(description);
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
