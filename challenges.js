190ee2b141c709aaa12aa03e5b1ff2a5514a5490
kevinsmba <kevinsmba@gmail.com>
2023-08-19 12:20:23 -0500

    adding the if condition to the challenge

--- challenges.js
+++ challenges.js
@@ -1,10 +1,16 @@
-/*const massMark = 95;
-const heighMark = 1.88;
-const massJohn = 85;
-const heighJohn = 1.76;
+const massMark = 78;
+const heighMark = 1.69;
+const massJohn = 92;
+const heighJohn = 1.95;
 
 const BMIMark = massMark / heighMark ** 2;
 const BMIJohn = massJohn / (heighJohn * heighJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);*/
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+if(markHigherBMI){
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's${BMIMark}!`)
+}
